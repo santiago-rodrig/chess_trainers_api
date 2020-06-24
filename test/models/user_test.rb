0975a6cfc1ec232381f00c2d3789e0c079766295
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-24 12:00:35 -0400

    test: user name uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'it creates a user' do
+    assert_difference 'User.count', 1 do
+      User.create(name: 'stuart')
+    end
+  end
+
+  test 'it does not create users with the same name' do
+    User.create(name: 'stuart')
+    assert_no_difference 'User.count' do
+      User.create(name: 'stuart')
+    end
+  end
 end
