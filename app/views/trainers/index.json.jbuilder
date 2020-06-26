1c5df8b232f75095ed532a9ff4b4c976d9ef7f03
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-26 16:57:42 -0400

    feature: cors headers gem

--- app/views/trainers/index.json.jbuilder
+++ app/views/trainers/index.json.jbuilder
@@ -1,6 +1,8 @@
 # app/views/trainers/index.json.jbuilder
 
-json.array! @trainers do |trainer|
+json.last_group @last_group
+
+json.trainers @trainers do |trainer|
   json.(trainer, :name, :events_won, :calendar_url, :location_url)
   json.expertise trainer.expertise.name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'trainers/group/:number', to: 'trainers#index'
+  get 'trainers/group/:number', to: 'trainers#index', default: { format: 'json' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
