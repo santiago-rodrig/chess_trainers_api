1de03382e8e895b2067bc1af2704afd04181cfbd
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-28 15:18:43 -0400

    fix: get to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'trainers/group/:number', to: 'trainers#index', default: { format: 'json' }
   get 'appointments/group/:number', to: 'appointments#index', default: { format: 'json' }
-  get 'login', to: 'users#login', default: { format: 'json' }
+  post 'login', to: 'users#login', default: { format: 'json' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
