9e1746e94a809e99817fc4b14ccd44c983055ba3
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-28 18:26:03 -0400

    feature: appointments creation

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,25 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.buffer(params[:number].to_i)
     @last_group = @appointments.last == Appointment.last
   end
+
+  def create
+    if data = is_data_valid?(params[:token], params[:trainer])
+      if Appointment.create(data)
+        head :ok
+      else
+        head :internal_server_error
+      end
+    else
+      head :conflict
+    end
+  end
+
+  private
+
+  def is_data_valid?(user_token, trainer_name)
+    user = User.find_by(token: user_token)
+    trainer = Trainer.find_by(name: trainer_name)
+    status = Appointment.find_by(name: 'pending')
+    user && trainer && { user: user, trainer: trainer, appointment_status: status }
+  end
 end
