98c4be648b1a94566b1f6b2ef65a6afe7410cb49
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-28 17:04:53 -0400

    feature: login users and check status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
   def login
     user = User.find_by(name: login_params[:name])
-    if user.authenticate(login_params[:password])
+    if user && user.authenticate(login_params[:password])
       token = Digest::SHA2.hexdigest(user.name.split('').shuffle.join(''))
       user.update_attribute(:token, token)
-      TokenCleanupJob.set(wait: 1.minute).perform_later(user)
+      TokenCleanupJob.set(wait: 1.day).perform_later(user)
       render json: { token: token }, status: :ok
+    else
+      head :unauthorized
+    end
+  end
+
+  def logged_in
+    answer = User.find_by(token: params[:token])
+    if answer
+      head :ok
     else
       head :unauthorized
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'trainers/group/:number', to: 'trainers#index', default: { format: 'json' }
   get 'appointments/group/:number', to: 'appointments#index', default: { format: 'json' }
   post 'login', to: 'users#login', default: { format: 'json' }
+  post 'logged_in', to: 'users#logged_in', default: { format: 'json' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
