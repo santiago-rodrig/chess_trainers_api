c17748d1295e9022ab16cb8c902ee04ec4b1518e
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-29 12:22:52 -0400

    fix: accept user email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def login_params
