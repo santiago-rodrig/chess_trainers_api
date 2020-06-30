31f74316c421471a4c1691f3a7fa3f89c8874e02
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-30 15:13:28 -0400

    feature: expert and intermediate filters

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,6 +1,18 @@
 class TrainersController < ApplicationController
   def index
-    @trainers = Trainer.buffer(params[:number].to_i, params[:tname])
-    @last_group = Trainer.filtered(params[:tname]) == @trainers.last
+    @trainers = Trainer.buffer(
+      params[:number].to_i,
+      params[:tname],
+      params[:texpert],
+      params[:tintermediate]
+    )
+
+    @last_group = Trainer.filtered(
+      params[:tname],
+      params[:texpert],
+      params[:tintermediate]
+    ).last == @trainers.last
+
+    byebug
   end
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -2,14 +2,60 @@ class Trainer < ApplicationRecord
   belongs_to :expertise
   has_many :appointments
 
-  scope :filtered, -> (name_filter) do
-    where("name LIKE ?", "%#{name_filter}%")
+  scope :filtered, -> (
+    name_filter,
+    expertFilter,
+    intermediateFilter
+  ) do
+    data = where("name LIKE ?", "%#{name_filter}%")
+
+    case expertFilter
+    when '1'
+      case intermediateFilter
+      when '1'
+        data = data.where(
+          expertise: Expertise.find_by(name: 'expert')
+        ).or(data.where(
+          expertise: Expertise.find_by(name: 'intermediate')
+        ))
+      when '0'
+        data = data.where(
+          expertise: Expertise.find_by(name: 'expert')
+        ).or(data.where.not(
+          expertise: Expertise.find_by(name: 'intermediate')
+        ))
+      end
+    when '0'
+      case intermediateFilter
+      when '1'
+        data = data.where.not(
+          expertise: Expertise.find_by(name: 'expert')
+        ).or(data.where(
+          expertise: Expertise.find_by(name: 'intermediate')
+        ))
+      when '0'
+        data = data.where.not(
+          expertise: Expertise.find_by(name: 'expert')
+        ).or(data.where.not(
+          expertise: Expertise.find_by(name: 'intermediate')
+        ))
+      end
+    end
+
+    data.order('events_won DESC')
   end
 
-  scope :buffer, -> (number, name_filter) do
-      filtered(name_filter)
-        .order('events_won DESC')
-        .offset(number * 3).first(3)
+  scope :buffer, -> (
+    number,
+    name_filter,
+    expertFilter,
+    intermediateFilter
+  ) do
+      filtered(
+        name_filter,
+        expertFilter,
+        intermediateFilter
+      ).offset(number * 3).first(3)
   end
 
   def hashed_email
