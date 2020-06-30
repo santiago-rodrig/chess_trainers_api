4fd4967598639b2a5b7212b3db59871575afa56f
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-30 11:48:42 -0400

    feature: filter trainer names

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,6 +1,6 @@
 class TrainersController < ApplicationController
   def index
-    @trainers = Trainer.buffer(params[:number].to_i)
-    @last_group = Trainer.last == @trainers.last
+    @trainers = Trainer.buffer(params[:number].to_i, params[:tname])
+    @last_group = Trainer.filtered(params[:tname]) == @trainers.last
   end
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,7 +1,16 @@
 class Trainer < ApplicationRecord
   belongs_to :expertise
   has_many :appointments
-  scope :buffer, -> (number) { order('events_won DESC').offset(number * 3).first(3) }
+
+  scope :filtered, -> (name_filter) do
+    where("name LIKE ?", "%#{name_filter}%")
+  end
+
+  scope :buffer, -> (number, name_filter) do
+      filtered(name_filter)
+        .order('events_won DESC')
+        .offset(number * 3).first(3)
+  end
 
   def hashed_email
     Digest::MD5.hexdigest(self.email)
