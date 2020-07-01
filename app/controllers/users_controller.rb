788f6c3cfdf02447142337091ec9f4c2740c7a85
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-01 19:01:20 -0400

    feature: show and update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,40 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    token = request.headers[:Authorization][7...]
+    user = User.find_by(token: token)
+
+    if user
+      render json: { name: user.name, email: user.email }, status: :ok
+    else
+      head :unauthorized
+    end
+  end
+
+  def update
+    token = request.headers[:Authorization][7...]
+    current_password = params[:current_password]
+    user = User.find_by(token: token)
+
+    data = {
+      name: params[:username],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    }
+
+    if user && user.authenticate(current_password)
+      if user.update(data)
+        render json: { username: user.name }, status: :ok
+      else
+        head :unprocessable_entity
+      end
+    else
+      head :unauthorized
+    end
+  end
+
   def create
     user = User.new(user_params)
     if user.save
