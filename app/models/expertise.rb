b5cfdd384d89986e9ed7077359eb764d0e465270
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-06 11:56:51 -0400

    fix: expertise validations

--- app/models/expertise.rb
+++ app/models/expertise.rb
@@ -1,3 +1,5 @@
 class Expertise < ApplicationRecord
   has_many :trainers
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: true
 end
--- test/models/expertise_test.rb
+++ test/models/expertise_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ExpertiseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	setup do
+		@expertise = Expertise.new(name: 'amateur')
+	end
+
+	test 'nothing wrong with valid data' do
+		assert @expertise.valid?
+	end
+
+  test 'name must be present' do
+		@expertise.name = nil
+
+		assert @expertise.invalid?  	
+  end
+
+  test 'name length must greater than 2' do
+  	@expertise.name = 'aa'
+
+  	assert @expertise.invalid?
+  end
+
+  test 'name lenght must be less than 21' do
+  	@expertise.name = 'a' * 21
+
+  	assert @expertise.invalid?
+  end
+
+  test 'name must be unique' do
+  	@expertise.name = expertises(:one).name
+
+  	assert @expertise.invalid?
+  end
 end
