347d9deeed46ff8dedb4e96783846ae85bbcdef4
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-06 12:32:55 -0400

    fix: user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :appointments
   has_secure_password
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :token, format: { with: /[A-Fa-f0-9]{64}/ }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+  	@user = users(:one)
+  end
+
+  test 'name must be preset' do
+  	@user.name = nil
+
+  	assert @user.invalid?
+  end
+
+  test 'name lenght must be greater than 2' do
+  	@user.name = 'aa'
+
+  	assert @user.invalid?
+  end
+
+  test 'name length must be less than 51' do
+  	@user.name = 'a' * 51
+
+  	assert @user.invalid?
+  end
+
+  test 'email must have a valid format' do
+  	@user.email = 'paralelepipedo'
+
+  	assert @user.invalid?
+  end
+
+  test 'name must be unique' do
+  	other_user = users(:two)
+  	@user.name = other_user.name
+
+  	assert @user.invalid?
+  end
+
+  test 'email must be unique' do
+  	other_user = users(:two)
+  	@user.email = other_user.email
+
+  	assert @user.invalid?
+  end
+
+  test 'token is present and is not a SHA256 hash' do
+  	@user.token = 'secret'
+
+  	assert @user.invalid?
+  end
+
+  test 'token is present and is a SHA256 hash' do
+  	@user.token = Digest::SHA2.hexdigest('randomness')
+
+  	assert @user.valid?
+  end
 end
