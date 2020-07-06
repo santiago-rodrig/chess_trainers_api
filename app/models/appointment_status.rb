1a07ab57a30062446be94f6f9316a7ab209c8210
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-06 13:19:33 -0400

    fix: appointment status validations

--- app/models/appointment_status.rb
+++ app/models/appointment_status.rb
@@ -1,3 +1,5 @@
 class AppointmentStatus < ApplicationRecord
   has_many :appointments
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: true
 end
--- test/models/appointment_status_test.rb
+++ test/models/appointment_status_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class AppointmentStatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @appointment_status = appointment_statuses(:pending)
+  end
+
+  test 'name must be present' do
+    @appointment_status.name = nil
+
+    assert @appointment_status.invalid?
+  end
+
+  test 'name length must be greater than 2' do
+    @appointment_status.name = 'aa'
+
+    assert @appointment_status.invalid?
+  end
+
+  test 'name length must be less than 21' do
+    @appointment_status.name = 'a' * 21
+
+    assert @appointment_status.invalid?
+  end
+
+  test 'name must be unique' do
+    @appointment_status.name = appointment_statuses(:success).name
+
+    assert @appointment_status.invalid?
+  end
 end
